93f0686e6a3157d021a5259f1dfa6c687245a762
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-12-02 17:12:48 +0700

    Implement /create and add node-dev

--- index.ts
+++ index.ts
@@ -1,14 +1,21 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 
-const app = express();
+const todoItems = [
+    { title: 'Learn TypeScript', complete: false },
+];
 
+const app = express();
 app.use(bodyParser.json());
+
 app.get('/list', (req, res) => {
-    const todoItems = [
-        { title: 'Learn TypeScript', complete: false },
-    ];
     res.json(todoItems);
+});
+
+app.post('/create', (req, res) => {
+    let item = req.body;
+    todoItems.push(item);
+    res.status(201).send();
 });
 
 app.listen(3000, () => console.log('listen on 3000'));
