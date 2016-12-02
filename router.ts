1b4a62a008012fcf871e5cdc5a8d285f3f426146
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-12-02 18:02:43 +0700

    Change Promise to async/await

--- router.ts
+++ router.ts
@@ -8,14 +8,15 @@ const todoItems = [
 
 const route = express.Router();
 
-route.get('/list', (req, res) => {
-    todoService.list().then((todoItems) => res.json(todoItems));
+route.get('/list', async (req, res) => {
+    const todoItems = await todoService.list();
+    res.json(todoItems)
 });
 
-route.post('/create', (req, res) => {
+route.post('/create', async (req, res) => {
     let item = req.body;
-    todoService.create(item).then(() => res.status(201).send());
-
+    await todoService.create(item);
+    res.status(201).send()
 });
 
 export function registerRoute(app) {
